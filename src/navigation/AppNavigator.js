7d1d8f73c475d346b34b0bec814924d392cec743
Malia Pickett <malia@Malias-MacBook-Air.local>
2024-12-17 14:14:36 -0800

    Add Dashboard screen with expense summaries and charts

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -5,8 +5,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // Screens
 import LoginScreen from "../screens/LoginScreen";
 import SignupScreen from "../screens/SignupScreen";
-import DashboardScreen from "../screens/DashboardScreen";
 import PasswordResetScreen from "../screens/PasswordResetScreen";
+import DashboardScreen from "../screens/DashboardScreen";
+import MileageTrackingScreen from "../screens/MileageTrackingScreen";
+import ReportsScreen from "../screens/ReportsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,41 +17,41 @@ const AppNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         {/* Authentication Screens */}
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-          options={{ headerShown: false }} 
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="Signup" 
-          component={SignupScreen} 
-          options={{ title: "Sign Up" }} 
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ title: "Sign Up" }}
         />
-        <Stack.Screen 
-          name="PasswordReset" 
-          component={PasswordResetScreen} 
-          options={{ title: "Reset Password" }} 
+        <Stack.Screen
+          name="PasswordReset"
+          component={PasswordResetScreen}
+          options={{ title: "Reset Password" }}
         />
 
         {/* Main App Screens */}
-        <Stack.Screen 
-          name="Dashboard" 
-          component={DashboardScreen} 
-          options={{ title: "Dashboard" }} 
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{ title: "Dashboard" }}
+        />
+        <Stack.Screen
+          name="MileageTracking"
+          component={MileageTrackingScreen}
+          options={{ title: "Mileage Tracking" }}
+        />
+        <Stack.Screen
+          name="Reports"
+          component={ReportsScreen}
+          options={{ title: "Reports" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-import MileageTrackingScreen from "../screens/MileageTrackingScreen";
-
-// Add the new screen to your navigator
-<Stack.Screen
-  name="MileageTracking"
-  component={MileageTrackingScreen}
-  options={{ title: "Mileage Tracking" }}
-/>;
-
 
 export default AppNavigator;
